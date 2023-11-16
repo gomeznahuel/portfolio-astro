67271bf5e9884db3952feb86e8a53e6d58efea80
Nahuel <ngomezpf@gmail.comm>
2023-11-16 19:08:13 -0300

    Update LinkedIn link in presentation

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -18,7 +18,7 @@ const presentation: Presentation = {
   description: "A *Backend developer* with over *3 years* of web experience, specializing in *JavaScript (Nest JS / Express JS), TypeScript and SQL*. My work spans a range of projects, including creating ecommerce websites and managing databases. I have *knowledge* in *Frontend*.",
   socials: [
     { label: "Github", link: "https://github.com/almanoduerme" },
-    { label: "LinkedIn", link: "https://www.linkedin.com/in/gomeznahuel/" },
+    { label: "LinkedIn", link: "https://www.linkedin.com/in/nahuelagomez/" },
     { label: "X", link: "https://twitter.com/gomeznahueldev" },
   ],
 };
